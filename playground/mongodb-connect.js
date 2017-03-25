4760a3e62cefbff55de36b5665308d67574171f3
Chris Woodruff <cjwoodru2@gmail.com>
2017-03-25 14:52:49 +0000

    add initial collection for mongo

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,22 @@
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect('mongodb://test:password@ds135800.mlab.com:35800/nodeapi', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB');
+    }
+    
+    console.log('Connected to be mLab DB Server');
+    
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to write data', err);
+        }
+        
+        console.log(JSON.stringify(result.ops, undefined, 2))
+    })
+    
+    db.close();
+})
